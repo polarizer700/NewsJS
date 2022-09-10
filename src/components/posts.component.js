4b5ea7f00252f59428223097aa8fb8ca7f1bf656
efimov.v <efimov.v@perfluence.io>
2022-09-10 10:36:43 +0300

    add loader

--- src/components/posts.component.js
+++ src/components/posts.component.js
@@ -3,14 +3,17 @@ import {apiService} from "../services/api.services";
 import {TransformService} from '../services/transform.service'
 
 export class PostsComponent extends Component{
-    constructor(id) {
+    constructor(id,{loader}) {
         super(id);
+        this.loader = loader;
     }
 
     async onShow(){
-       const fbData =  await apiService.fetchPost()
-       const posts =  TransformService.fbObjectToArray(fbData)
+        this.loader.show();
+        const fbData =  await apiService.fetchPost()
+        const posts =  TransformService.fbObjectToArray(fbData)
         const html = posts.map(post => renderPost(post)).join(' ')
+        this.loader.hide();
         this.$el.insertAdjacentHTML('afterbegin',html)
     }
 
@@ -20,8 +23,15 @@ export class PostsComponent extends Component{
 }
 
 function renderPost(post){
-    const tag = post.type === 'news' ? '<li class="tag tag-blue tag-rounded">Новость</li>' :
-       '<li className="tag tag-rounded">Заметка</li>';
+    let tag = '';
+    if(post.type === 'news'){
+        tag = '<li class="tag tag-blue tag-rounded">Новость</li>';
+    }else if (post.type === 'note'){
+        tag = '<li class="tag tag-rounded">Заметка</li>';
+    }else if(post.type === 'internet'){
+        tag = '<li class="tag tag-rounded">Интернет</li>';
+    }
+
 
     const button = '<button class="button-round button-small button-primary">Сохранить</button>'
     return `<div class="panel">
